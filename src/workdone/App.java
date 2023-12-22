f40cf62723dc945924e0af376b731fc4e0f76ccb
Ajay Rawat <arj69619@gmail.com>
2023-12-22 18:12:07 +0530

    initial commit

---
+++ src/workdone/App.java
@@ -0,0 +1,15 @@
+package workdone;
+
+import javax.swing.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new WorkDone().setVisible(true);
+			}
+		});
+	}
+}
